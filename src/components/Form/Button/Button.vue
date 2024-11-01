ba889a8f091a881aa079df8bdd7749f803a8c3a4
AdrianResende <adrianresende147@gmail.com>
2024-11-01 14:00:27 -0300

    feat: creating component Button

---
+++ src/components/Form/Button/Button.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-facing-decorator';
+
+
+@Component({
+  name: "Button"
+})
+export default class Button extends Vue {
+  @Prop({ default: "Button" }) name!: string;
+}
+</script>
+
+<template>
+  <v-btn> {{ name }}</v-btn>
+</template>
