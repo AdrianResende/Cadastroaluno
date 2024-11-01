80558ef01bc3b0431e81a2f27c5eebb846087cbb
AdrianResende <adrianresende147@gmail.com>
2024-11-01 14:00:18 -0300

    feat: creating component Form

---
+++ src/components/Form/Card.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-facing-decorator';
+
+
+@Component({
+  name: "Card"
+})
+export default class Card extends Vue {
+}
+</script>
+
+<template>
+  <v-dialog>
+    <v-card>
+      <v-card-title>
+        <slot name="title"></slot>
+      </v-card-title>
+      <v-card-text>
+        <slot name="content"></slot>
+      </v-card-text>
+      <v-card-actions>
+        <slot name="actions"></slot>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
